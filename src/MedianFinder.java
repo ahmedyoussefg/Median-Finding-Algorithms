fa7ab4b0ef6bbd21ef470c57e12fa16ca0b1cf57
ahmedyoussefg <a.goerany@gmail.com>
2024-10-30 21:21:31 +0300

    Add plotter code

--- src/MedianFinder.java
+++ src/MedianFinder.java
@@ -16,19 +16,19 @@ public abstract class MedianFinder {
 
     protected int[] partition(int l, int r) {
         int pivot = this.dataPoints[l]; // let the pivot be the first element
-        int lt = l; // pointer after less than partition
-        int gt = r; // pointer before greater than partition
-        for (int i = l+1; i <= gt ; ) {
+        int less = l; // pointer after less than partition
+        int greater = r; // pointer before greater than partition
+        for (int i = l+1; i <= greater ; ) {
             if (this.dataPoints[i] < pivot) {
-                // if current element is less than pivot, we add it to less than partition and increase lt
-                swapDataPoints(lt, i);
-                lt++;
+                // if current element is less than pivot, we add it to less than partition and increase "less"
+                swapDataPoints(less, i);
+                less++;
                 i++; // go to next element
             }
             else if (this.dataPoints[i] > pivot) {
-                // if current element is greater than pivot, we add it to greater than partition and decrease gt
-                swapDataPoints(gt, i);
-                gt--;
+                // if current element is greater than pivot, we add it to greater than partition and decrease "greater"
+                swapDataPoints(greater, i);
+                greater--;
             }
             else {
                 // if current element is equal to the pivot we skip it
@@ -36,7 +36,7 @@ public abstract class MedianFinder {
             }
         }
         // we return range of the equal-to-the-pivot elements
-        return new int[]{lt,gt};
+        return new int[]{less, greater};
     }
 
     // helper function to swap two elements in the dataPoints array, at index i and index j
